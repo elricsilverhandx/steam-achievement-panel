d82cbedc9e68c8ce3dee361c81d809d14978b9d6
Elric Silverhand <Byramklc@gmail.com>
2026-05-27 20:09:47 +0300

    Package Steam DLL on Windows

--- scripts/package-windows.cmd
+++ scripts/package-windows.cmd
@@ -4,5 +4,7 @@ set ARTIFACT=%2
 if exist dist rmdir /s /q dist
 mkdir dist\%ARTIFACT%
 copy target\%TARGET%\release\steam-achievement-panel.exe dist\%ARTIFACT%\steam-achievement-panel.exe
+for /r target\%TARGET%\release\build %%f in (steam_api64.dll) do copy "%%f" dist\%ARTIFACT%\steam_api64.dll
 echo Steam Achievement Panel> dist\%ARTIFACT%\README.txt
+echo Start Steam first, then run steam-achievement-panel.exe.>> dist\%ARTIFACT%\README.txt
 tar -a -c -f dist\%ARTIFACT%.zip -C dist %ARTIFACT%
